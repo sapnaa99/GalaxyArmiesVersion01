d910e2068f58ad833ada0792ab21873dd43cf2c5
sapnaa99 <sapnaa.chaudhary@gmail.com>
2020-06-27 21:01:53 +0545

    third commit

---
+++ GalaxyArmies.Data/Interface/IGalaxyArmies.cs
@@ -0,0 +1,16 @@
+﻿using GalaxyArmies.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalaxyArmies.Data.Interface
+{
+    public interface IGalaxyArmies
+    {
+        IEnumerable<GalaxyArmiesModel> GetAllGalaxyArmiesName(string name);
+        GalaxyArmiesModel GetById(long Id);
+        GalaxyArmiesModel Update(GalaxyArmiesModel updatedGalaxyArmiesModel);
+        GalaxyArmiesModel AddNew(GalaxyArmiesModel newGalaxyArmiesModel);
+        int Commit();
+    }
+}
